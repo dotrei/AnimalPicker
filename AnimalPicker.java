765b86b585cc97b38939ec2dfb6c445c7ec998be
reZdotjson <0083rei@proton.me>
2025-02-24 17:23:05 -0500

    commit 1

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
 public class AnimalPicker {
     public static void main(String[] args) {
 
-        System.out.println("CAT ASCII ART HERE");
+        Scanner input = new Scanner(System.in);
+
+        String in = input.nextLine().toUpperCase();
+        if (in == "cat") {
+            System.out.println("      |\\      _,,,---,,_\r\n" + //
+                                "ZZZzz /,`.-'`'    -.  ;-;;,_\r\n" + //
+                                "     |,4-  ) )-,_. ,\\ (  `'-'\r\n" + //
+                                "    '---''(_/--'  `-'\\_)  Felix Lee ");
+        } else if (in == "dog")  {
+            System.out.println("  .\r\n" + //
+                                " ..^____/\r\n" + //
+                                "`-. ___ )\r\n" + //
+                                "  ||  || mh");
+        } else if (in == "fish") {
+            System.out.println("|\\   \\\\\\\\__     o\r\n" + //
+                                "| \\_/    o \\    o \r\n" + //
+                                "> _   (( <_  oo  \r\n" + //
+                                "| / \\__+___/      \r\n" + //
+                                "|/     |/");
+        } else {
+            System.out.println("Invalid Input");
+        }
     }
 }
